7fb65b649490ba6392112a65a278fc7ae9dba645
rt7.wonjin <rt7.wonjin@gmail.com>
2022-03-26 18:51:50 +0900

    1
    1

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const ethereumButton = document.querySelector('.enableEthereumButton');
+const showAccount = document.querySelector('.showAccount');
 
 ethereumButton.addEventListener('click', () => {
-//Will Start the metamask extension
-ethereum.request({ method: 'eth_requestAccounts' });
+  getAccount();
 });
+
+async function getAccount() {
+  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+  const account = accounts[0];
+  showAccount.innerHTML = account;
+}
+
+ethereumButton2.addEventListener('click', () => {
+  getAccount2();
+});
+
+async function getAccount2() {
+  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+  const account = accounts[0];
+  showAccount.innerHTML = account;
+}
